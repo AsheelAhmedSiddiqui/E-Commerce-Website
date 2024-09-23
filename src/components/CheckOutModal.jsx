6305d7b667bf9154ad6bc2892a4ba2314632952a
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-09-23 22:21:55 +0500

    auth

--- src/components/CheckOutModal.jsx
+++ src/components/CheckOutModal.jsx
@@ -1,17 +1,16 @@
 import { Button, Modal } from "antd";
 import { useEffect, useState } from "react";
 import { GoogleOutlined } from "@ant-design/icons";
-import { auth } from "./firebase/auth";
+import { auth } from "../utils/firebase";
 function CheckOutModal({ isOpen, handleConfirm, handleCancel }) {
 	// orderConfirm as props
-
 	const [guestUser, setGuestUser] = useState(false);
 	useEffect(() => {
 		return setGuestUser(false);
 	}, []);
 
-	console.log(auth);
-
+	// console.log(getAuth(app));
+	const isLogin = auth.currentUser;
 	return (
 		<>
 			<Modal
@@ -24,13 +23,38 @@ function CheckOutModal({ isOpen, handleConfirm, handleCancel }) {
 				footer={null}
 				width={800}
 			>
-				<div className="flex items-center justify-center flex-col gap-6 my-10">
-					<Button className="py-6 px-8">
-						<GoogleOutlined /> Continue as Google
-					</Button>
-					<p>---------- OR ----------</p>
-					<Button className="py-6 px-8">Continue as Guest</Button>
-				</div>
+				{!isLogin && !guestUser && (
+					<div className="flex items-center justify-center flex-col gap-6 my-10">
+						<Button className="py-6 px-8">
+							<GoogleOutlined /> Continue as Google
+						</Button>
+						<p>---------- OR ----------</p>
+						<Button className="py-6 px-8">Continue as Guest</Button>
+					</div>
+				)}
+				{isLogin ||
+					(guestUser && (
+						<Form onFinish={checkoutOrder} layout="vertical">
+							<Form.Item name={"username"} label={"Username"}>
+								<Input />
+							</Form.Item>
+							<Form.Item name={"email"} required label={"Email"}>
+								<Input type="email" />
+							</Form.Item>
+							<Form.Item name={"number"} required label={"Phone Number"}>
+								<Input type="number" />
+							</Form.Item>
+							<Form.Item required name={"address"} label={"Address"}>
+								<Input.TextArea placeholder="Address" />
+							</Form.Item>
+
+							<Form.Item>
+								<Button type="primary" htmlType="submit">
+									Submit
+								</Button>
+							</Form.Item>
+						</Form>
+					))}
 			</Modal>
 		</>
 	);
